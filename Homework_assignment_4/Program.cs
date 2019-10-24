e5cd736e517c53496cac391802e25f4df72f24fd
Anu <anniks_4@hotmail.com>
2019-10-24 19:05:06 +0300

    Array

---
+++ Homework_assignment_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework_assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numbers = new int [100];
+            int sumOfNumbers = 0;
+
+            for (int i=0; i < 100; i++)
+            {
+                numbers[i] = i + 1;
+                sumOfNumbers = sumOfNumbers + i;
+               
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine($"Sum of numbers 1 to 100 is {sumOfNumbers}.");
+       
+        }
+
+    }
+}
